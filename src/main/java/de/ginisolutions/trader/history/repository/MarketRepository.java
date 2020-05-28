48939025a376cc57642bd97f7149533e580b2069
Jakob Erpf <contact@jakoberpf.de>
2020-05-28 17:38:45 +0200

    adding imples

--- src/main/java/de/ginisolutions/trader/history/repository/MarketRepository.java
+++ src/main/java/de/ginisolutions/trader/history/repository/MarketRepository.java
@@ -2,9 +2,12 @@ package de.ginisolutions.trader.history.repository;
 
 import de.ginisolutions.trader.history.domain.Market;
 
+import de.ginisolutions.trader.history.domain.enumeration.MARKET;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * Spring Data MongoDB repository for the Market entity.
@@ -12,4 +15,8 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface MarketRepository extends MongoRepository<Market, String> {
+
+    Optional<Market> findByMarket(MARKET market);
+
+    boolean existsByMarket(MARKET market);
 }
